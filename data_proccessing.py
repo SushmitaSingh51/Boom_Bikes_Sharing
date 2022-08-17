f1bb3b684d20c65dba25af89d36aa218502e240c
SushmitaSingh51 <108569587+SushmitaSingh51@users.noreply.github.com>
2022-08-17 15:17:34 +0530

    Update data_proccessing.py

--- data_proccessing.py
+++ data_proccessing.py
@@ -1,1 +1,16 @@
 print("all data_processing code related Boom_Bikes_Sharing")
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+import warnings
+warnings.filterwarnings('ignore')
+import pandas as pd
+import pandas as pd
+path = ''
+dataset = pd.read_csv(path+'bike_sharing_data.csv')
+import os
+path = ''
+##Data Preparation
+df = pd.read_csv(r" ")
+df.head()
